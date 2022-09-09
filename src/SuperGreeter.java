6283be06df67242ab1e62174b6f5a1c94e370fcd
sami0880-kea <sami0880@stud.kea.dk>
2022-09-09 09:59:27 +0200

    added askAboutAge

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class SuperGreeter {
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         SuperGreeter SG = new SuperGreeter();
         SG.start();
         SG.greet();
+        SG.askAboutAge();
     }
 
     public void start() {
@@ -12,9 +14,14 @@ public class SuperGreeter {
     }
 
     public void greet() {
-        Scanner sc = new Scanner(System.in);
         System.out.print("Indtast dit navn: ");
         String navn = sc.next();
         System.out.println("Hej med dig, " + navn);
+    }
+
+    public void askAboutAge() {
+        System.out.print("Indtast din alder: ");
+        int alder = sc.nextInt();
+        System.out.println("Du er " + alder + " år gammel.");
     }
 }
