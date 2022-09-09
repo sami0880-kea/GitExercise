1d8ce10a8b9dcf5ad3eef052df878fb1bdb97d31
sami0880-kea <sami0880@stud.kea.dk>
2022-09-09 09:57:30 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public static void main(String[] args) {
         SuperGreeter SG = new SuperGreeter();
         SG.start();
+        SG.greet();
     }
 
     public void start() {
         System.out.println("Det virker!");
+    }
+
+    public void greet() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Indtast dit navn: ");
+        String navn = sc.next();
+        System.out.println("Hej med dig, " + navn);
     }
 }
