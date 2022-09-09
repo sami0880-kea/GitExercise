3211b3fe324baa04d87a39cad77b68f6ec546819
sami0880-kea <sami0880@stud.kea.dk>
2022-09-09 09:54:56 +0200

    initial commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,10 @@
+public class SuperGreeter {
+    public static void main(String[] args) {
+        SuperGreeter SG = new SuperGreeter();
+        SG.start();
+    }
+
+    public void start() {
+        System.out.println("Det virker!");
+    }
+}
